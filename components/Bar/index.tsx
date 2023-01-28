2db792c4c58df22803d7d337a121604545dcb668
ryanjwong <ryanjwong007@gmail.com>
2023-01-28 00:18:08 -0800

    More styles

--- components/Bar/index.tsx
+++ components/Bar/index.tsx
@@ -2,14 +2,21 @@ import React from "react";
 import { Navbar, Button, Link, Text, Card, Radio, useTheme } from "@nextui-org/react";
 import { Logo } from "../Logo"
 export default function Bar() {
-
+  const collapseItems = [
+    "HOME",
+    "LITEPAPER",
+    "TEAM",
+    "CONTACT US",
+  ];
   return (
        <Navbar isBordered={true} variant={"sticky"}  css={{
         background: "transparent",
         $$navbarContainerMaxWidth: "100%",
-  $$navbarBackgroundColor: "transparent",
-  $$navbarBlurBackgroundColor: "bg-neutral-800"
+        $$navbarBackgroundColor: "transparent",
+        $$navbarBlurBackgroundColor: "bg-neutral-800",
+        $$navBarTextColor: "white",
 }}>
+        <Navbar.Toggle showIn="xs"/>
 
         <Navbar.Brand>
           <Text b color="white" hideIn="xs">
@@ -18,7 +25,7 @@ export default function Bar() {
         </Navbar.Brand>
         <Navbar.Content activeColor={"primary"} hideIn="xs" variant={"underline-rounded"}>
           <Navbar.Link isActive href="#" css={{color: "white"}}>Home</Navbar.Link>
-          <Navbar.Link href="#" css={{color: "white"}}>Litepaper</Navbar.Link>
+          <Navbar.Link animated={false} href="#" css={{color: "white"}}>Litepaper</Navbar.Link>
           <Navbar.Link href="#" css={{color: "white"}}>Team</Navbar.Link>
           <Navbar.Link href="#" css={{color: "white"}}>Contact Us</Navbar.Link>
         </Navbar.Content>
@@ -32,6 +39,31 @@ export default function Bar() {
             </Button>
           </Navbar.Item>
         </Navbar.Content>
+        <Navbar.Collapse 
+          css={{
+            background: "rgba(0, 0, 0, 0.4)"
+        }}>
+          {collapseItems.map((item, index) => (
+            <Navbar.CollapseItem
+              key={item}
+              activeColor="primary"
+              css={{
+                color: "white",
+              }}
+              isActive={index === 2}
+            >
+              <Link
+                color="inherit"
+                css={{
+                  minWidth: "100%",
+                }}
+                href="#"
+              >
+                {item}
+              </Link>
+            </Navbar.CollapseItem>
+          ))}
+        </Navbar.Collapse>
       </Navbar>
       
   )
--- components/Content/index.tsx
+++ components/Content/index.tsx
@@ -7,11 +7,7 @@ export const Content = () => (
           Cloud Computing - Reinvented
     </Text>
     <Text color="white" size="$lg" blockquote>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-      labore et dolore magna aliqua. Purus gravida quis blandit turpis. Augue neque gravida in
-      fermentum et sollicitudin ac orci. Et sollicitudin ac orci phasellus egestas. Elementum tempus
-      egestas sed sed risus pretium quam vulputate. Interdum velit euismod in pellentesque massa
-      placerat duis ultricies.
+      The future of Cloud Computing is distributed.
     </Text>
     <Spacer y={1} />
     <Text color="white" size="$lg">
